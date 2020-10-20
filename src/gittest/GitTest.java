279e5fb1e57bb44491ab56edfaf82183529d4d39
Yok <yokkanjanaporn@gmail.com>
2020-10-20 17:06:42 +0700

    newfeature

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -13,8 +13,12 @@ public class GitTest {
 
     public static void main(String[] args) {
         sayHello();
+        sayGoodBye();
     }
     public static void sayHello(){
         System.out.println("Hello");
+    }
+    public static void sayGoodBye(){
+        System.out.println("Goodbye");
     }
 }
